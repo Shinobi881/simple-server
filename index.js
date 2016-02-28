c9db97c031055ec70e0592bbafcbc9be9d6bee10
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-02-28 00:41:49 -0800

    (test) 404

--- index.js
+++ index.js
@@ -2,25 +2,24 @@ var express = require('express');
 var app = express();
 var port = process.env.port || 3000;
 
-var options = {
-    root: __dirname + '/',
-    dotfiles: 'deny',
-    headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true
-    }
-  };
-
 app.use(express.static(__dirname + '/'));
 app.get('/', function (res, res) {
   res.sendFile(__dirname + '/index.html', function (err) {
     if (err) {
       res.status(500).send(err);
     }
-  })
+  });
 });
 
-app.listen(port, function (argument) {
+app.get('/test', function (res, res) {
+  res.sendFile(__dirname + '/404.html', function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.listen(port, function () {
    console.log('listening on port', port);
 });
 
